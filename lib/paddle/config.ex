d53a32319a213aa28b16322b0d682cfdcf46784e
Ricardo Ramos <rcdo.ramos@gmail.com>
2021-03-20 13:51:05 -0300

    Fix typo in config resolution

--- lib/paddle/config.ex
+++ lib/paddle/config.ex
@@ -3,7 +3,7 @@ defmodule Paddle.Config do
     %{
       vendor_id: Application.get_env(:paddlex, :vendor_id),
       vendor_auth_code: Application.get_env(:paddlex, :vendor_auth_code),
-      environment: Application.get_env(:paddlex, :enviroment)
+      environment: Application.get_env(:paddlex, :environment)
     }
   end
 end
